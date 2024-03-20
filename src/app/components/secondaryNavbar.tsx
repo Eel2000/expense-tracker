4e65f12a448190e6b73a7addc410b36b9a770c0f
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2024-03-20 11:07:02 +0200

    fixe links reloading

--- src/app/components/secondaryNavbar.tsx
+++ src/app/components/secondaryNavbar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function SecondaryNavbar(){
     return(
         <header className="absolute inset-x-0 top-0 z-50">
             <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
+                        <span className="sr-only">E-tracker</span>
                         <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
                     </a>
                 </div>
@@ -17,13 +19,13 @@ export default function SecondaryNavbar(){
                     </button>
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Product</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Features</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Company</a>
+                    <Link href="/modules/dashboard/" className="text-sm font-semibold leading-6 text-gray-900">Dashboard</Link>
+                    <Link href="/modules/expenses/" className="text-sm font-semibold leading-6 text-gray-900">Features</Link>
+                    <Link href="/modules/services/" className="text-sm font-semibold leading-6 text-gray-900">Services</Link>
+                    <Link href="/modules/about/" className="text-sm font-semibold leading-6 text-gray-900">About</Link>
                 </div>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+                    <Link href="/auth/login/" className="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></Link>
                 </div>
             </nav>
             {/*Mobile menu, show/hide based on menu open state.*/}
@@ -32,10 +34,10 @@ export default function SecondaryNavbar(){
                 <div className="fixed inset-0 z-50"></div>
                 <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                        <Link href="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">E-Tracker</span>
                             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
-                        </a>
+                        </Link>
                         <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
                             <span className="sr-only">Close menu</span>
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
@@ -46,13 +48,13 @@ export default function SecondaryNavbar(){
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
-                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
-                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+                                <Link href="../modules/dashboard/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Dashboard</Link>
+                                <Link href="../modules/expenses/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</Link>
+                                <Link href="../modules/services/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Services</Link>
+                                <Link href="../modules/about/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</Link>
                             </div>
                             <div className="py-6">
-                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+                                <Link href="../auth/login/" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</Link>
                             </div>
                         </div>
                     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
         <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -13,7 +11,7 @@ export default function Home() {
                 <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                     <div
                         className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                        Announcing our next round of funding. <a href="#"
+                        Announcing our next round of pricing. <a href="modules/pricings"
                                                                  className="font-semibold text-indigo-600"><span
                         className="absolute inset-0" aria-hidden="true"></span>Read more <span
                         aria-hidden="true">&rarr;</span></a>
@@ -27,10 +25,10 @@ export default function Home() {
                         sunt.
                         Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#"
+                        <a href="/modules/expenses"
                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
                             started</a>
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span
+                        <a href="/modules/about" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span
                             aria-hidden="true">→</span></a>
                     </div>
                 </div>
