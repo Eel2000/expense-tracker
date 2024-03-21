b4f0542449228396413f720fd6f87cdc8f49c0f1
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2024-03-21 16:56:03 +0200

    table component

---
+++ src/app/components/cardTable.tsx
@@ -0,0 +1,52 @@
+import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip} from "@nextui-org/react";
+import {BankCard} from "@/app/lib/models/bankCard";
+import React from "react";
+import {DeleteIcon, EditIcon, EyeIcon} from "@nextui-org/shared-icons";
+
+export default function CardTable({cards}: { cards: BankCard[] }) {
+
+    const formatDate = (date: Date) => {
+        return date.toDateString()
+    }
+
+    return (
+        <Table removeWrapper aria-label="Example static collection table">
+            <TableHeader>
+                <TableColumn>NUMBER</TableColumn>
+                <TableColumn>SOLD</TableColumn>
+                <TableColumn>ADDED DATE</TableColumn>
+                <TableColumn>ACTIONS</TableColumn>
+            </TableHeader>
+            <TableBody emptyContent={"No data to display."}>
+                {
+                    cards.map(c =>
+                        <TableRow key={c.id}>
+                            <TableCell>{c.cardNumber}</TableCell>
+                            <TableCell>{c.sold}</TableCell>
+                            <TableCell>{formatDate(c.addedOn)}</TableCell>
+                            <TableCell>
+                                <div className="relative flex items-center gap-2">
+                                    <Tooltip content="Details">
+                                      <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                        <EyeIcon/>
+                                      </span>
+                                    </Tooltip>
+                                    <Tooltip content="Edit user">
+                                      <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                        <EditIcon/>
+                                      </span>
+                                    </Tooltip>
+                                    <Tooltip color="danger" content="Delete user">
+                                      <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                                        <DeleteIcon/>
+                                      </span>
+                                    </Tooltip>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    )
+                }
+            </TableBody>
+        </Table>
+    )
+}
--- src/app/modules/services/cards/page.tsx
+++ src/app/modules/services/cards/page.tsx
@@ -4,6 +4,22 @@ import {NewBankCard} from "@/app/lib/models/newBankCard";
 import {ChangeEvent, useState} from "react";
 import {BankCard} from "@/app/lib/models/bankCard";
 import Swal from 'sweetalert2'
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    useDisclosure,
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell
+} from "@nextui-org/react";
+import {Button} from "@nextui-org/button";
+import CardTable from "@/app/components/cardTable";
 
 export default function Page() {
     const crd: NewBankCard = {
@@ -13,6 +29,7 @@ export default function Page() {
         addedOn: new Date()
     }
 
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
     const [newCard, setNewCard] = useState<NewBankCard>(crd)
     const [cards, setCards] = useState<BankCard[]>([])
 
@@ -41,46 +58,68 @@ export default function Page() {
         })
 
         console.log(card)
+        alert(card, "data sent")
+        onOpenChange()
 
+        setNewCard(crd)
     }
 
-    const openForm = () => {
+    const alert = (data: any, message: string) => {
         Swal.fire({
-            title:'<div>text</div>',
-            html:'<div>' +
-                '<input type="search" class="rounded border-1 outline-amber-200">' +
-                '</div>',
-            allowOutsideClick: false
-        }).catch((error) => console.log(error))
+            title: "Good job!",
+            text: `${message} : ${JSON.stringify(data)}`,
+            icon: "success"
+        }).then(() => console.log('done'))
     }
 
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>cards</h1>
-            <button onClick={openForm}>test</button>
-            <form action={saveCard} className="grid gap-5 w-[60%]">
-                <div className="grid grid-cols-1 gap-2">
-                    <label>Card Number</label>
-                    <input className="w-full p-2 active:border-blue-600 focus:outline-blue-600 rounded"
-                           type="text" name="cardNumber" value={newCard.cardNumber} onChange={handleTodoChanges}
-                           placeholder="enter the amount" aria-required="true" required={true}/>
-                </div>
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <h1 className="text-3xl">Cards - Management</h1>
+            <Button color="primary" variant="solid" className="my-4" onPress={onOpen}>Add new card</Button>
+            <CardTable cards={cards}/>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false} isKeyboardDismissDisabled={true}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1 mx-5">Bank Card informations</ModalHeader>
+                            <ModalBody>
+                                <form action={saveCard} className="grid gap-5 w-full px-5">
+                                    <div className="grid grid-cols-1 gap-2">
+                                        <label>Card Number</label>
+                                        <input
+                                            className="w-full p-2 outline outline-1 active:border-blue-600 focus:outline-blue-600 rounded"
+                                            type="text" name="cardNumber" value={newCard.cardNumber}
+                                            onChange={handleTodoChanges}
+                                            placeholder="enter the amount" aria-required="true" required={true}/>
+                                    </div>
 
-                <div className="grid grid-cols-1 gap-2">
-                    <label>Sold ( $ )</label>
-                    <input className="w-full p-2 focus:border-none focus:outline-blue-600 rounded"
-                           type="number" name="sold" value={newCard.sold} onChange={handleTodoChanges}
-                           placeholder="enter the amount" aria-required="true" required={true}/>
-                </div>
+                                    <div className="grid grid-cols-1 gap-2">
+                                        <label>Sold ( $ )</label>
+                                        <input
+                                            className="w-full p-2 outline outline-1 focus:border-none focus:outline-blue-600 rounded"
+                                            type="number" name="sold" value={newCard.sold}
+                                            onChange={handleTodoChanges}
+                                            placeholder="enter the amount" aria-required="true" required={true}/>
+                                    </div>
 
-                <div className="grid grid-cols-2 gap-2">
-                    <input type="submit" value="Save"
-                           className="rounded bg-blue-600 h-[150%] active:bg-blue-700 text-white"/>
-                    <input type="reset" value="cancel & clean"
-                           className="rounded bg-gray-600 h-[150%] active:bg-gray-700 text-white"/>
-                </div>
-            </form>
+                                    <div className="grid grid-cols-2 gap-2">
+                                        <input type="submit" value="Save"
+                                               className="rounded bg-blue-600 h-[150%] active:bg-blue-700 text-white"/>
+                                        <input type="reset" value="cancel & clean"
+                                               className="rounded bg-gray-600 h-[150%] active:bg-gray-700 text-white"/>
+                                    </div>
+                                </form>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Close
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </main>
     )
 }
