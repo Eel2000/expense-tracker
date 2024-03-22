d5f20067bee0df39f7f9a7d688f42a00106ba503
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2024-03-22 16:04:08 +0200

    minor update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="relative isolate px-6 lg:px-8">
             <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                  aria-hidden="true">
                 <div
@@ -25,7 +25,7 @@ export default function Home() {
                         sunt.
                         Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="/modules/expenses"
+                        <a href="/modules/dashboard"
                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
                             started</a>
                         <a href="/modules/about" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span
